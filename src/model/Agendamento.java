9bd97d6d1398a6c2d312fe5bbb8a4958b3db7cd0
HarisonHeimVF <harisonheim@gmail.com>
2025-05-17 14:31:26 -0300

    [RF010] Criar classe Agendamento com atributos e método exibirInformacoes()

---
+++ src/model/Agendamento.java
@@ -0,0 +1,39 @@
+package model;
+
+import model.Cliente;
+import model.Servico;
+import model.FormaPagamento;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+
+
+public class Agendamento {
+    public Cliente cliente;
+    public Servico servico;
+    public Date data;
+    public String horario;
+    public FormaPagamento formaPagamento;
+    public int parcelas; // se for crédito
+    public double valorRecebido; // se for espécie
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public void exibirInformacoes() {
+        System.out.println("Cliente: " + cliente.nome + " (" + cliente.placa + ")");
+        System.out.println("Serviço: " + servico.nome);
+
+        System.out.println("Data: " + sdf.format(data) + " | Horário: " + horario);
+        System.out.println("Pagamento: " + formaPagamento);
+
+        if (formaPagamento == FormaPagamento.CREDITO && parcelas > 1) {
+            System.out.printf("Parcelado em %d vezes de R$ %.2f\n", parcelas, servico.preco / parcelas);
+        }
+
+        if (formaPagamento == FormaPagamento.ESPECIE && valorRecebido > servico.preco) {
+            double troco = valorRecebido - servico.preco;
+            System.out.printf("Valor recebido: R$ %.2f | Troco: R$ %.2f\n", valorRecebido, troco);
+        }
+    }
+}
