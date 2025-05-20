857660bdf0080ef2435ffb3c1fc1755a5549246e
AlbertoLucas04 <abertolucas12@gmail.com>
2025-05-20 18:39:16 -0300

    [RF020]Criar AvaliacaoService com metodos com cadastrar() e listar()

---
+++ src/Service/AvaliacaoService.java
@@ -0,0 +1,19 @@
+package Service;
+import model.Avaliacao;
+import Repository.AvaliacaoRepository;
+import java.util.List;
+
+
+public class AvaliacaoService {
+
+    private AvaliacaoRepository repository = new AvaliacaoRepository();
+
+    public void cadastrar(Avaliacao avaliacao) {
+        repository.salvar(avaliacao);
+    }
+
+    public List<Avaliacao> listar() {
+        return repository.listar();
+    }
+
+}
